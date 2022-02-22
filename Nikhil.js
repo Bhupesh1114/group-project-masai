d19698f718a32f79390b1128ca8df8f8c3f72ce1
NikhilMehara <mehranikhil1999@gmail.com>
2022-02-22 23:33:49 +0530

    Update Nikhil.js

--- Nikhil.js
+++ Nikhil.js
@@ -1,6 +1,8 @@
-var addiv = {
-    vimg = "",
-    head = "",
-    points = "",
-    btn = "",
-}
+
+var btn = document.querySelector("#change");
+btn.addEventListener("mouseover", function myfun(event){
+    event.target.style.backgroundColor = "yellow";
+},false);
+
+
+
