900e95b0265734aa308bd4ddec315a7edacefaa3
NikhilMehara <mehranikhil1999@gmail.com>
2022-02-25 21:06:25 +0530

    add

--- home.js
+++ home.js
@@ -1,13 +1,30 @@
 var a;
-document.getElementById("write").style.display = "none";
+var arr = JSON.parse(localStorage.getItem("store"))||[];
+var give = document.getElementById("write");
+console.log(give);
+give.style.display = "flex";
 function show_hide(){
-       if(a==1){
-           document.getElementById("write").style.display = "none";
-           return a=0;
-       }
+       
+    var dataObj = {task:give.value};
+    console.log(dataObj);
+    arr.push(dataObj);
+    console.log(arr);
+    localStorage.setItem("store",JSON.stringify(arr));
+    // console.log(localStorage.setItem("store",JSON.stringify(arr)));
 
-       else{
-           document.getElementById("write").style.display = "flex";
-           return a=1;
-       }
+    document.getElementById("show").innerHTML = give.value;
+    give.value = "";
+    if(a==1){
+        give.style.display = "flex";
+        return a=0;
     }
+
+    else{
+        give.style.display = "none";
+        return a=1;
+    }
+    
+    }
+
+
+    
